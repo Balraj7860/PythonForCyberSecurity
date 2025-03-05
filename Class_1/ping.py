dba2b7691613a4c42417e63d620e9504565b9188
Balraj Singh <bhundal2001@gmail.com>
2025-03-05 18:13:25 +0000

    Did more

--- Class_1/ping.py
+++ Class_1/ping.py
@@ -1,4 +1,10 @@
 import platform
 import os
 
+print("NORMAL\n")
+#normally
 os.system("ping -c 1 -w 2 8.8.8.8")
+print("\nREDIRECTING\n")
+
+#redirecting output output should be zero but mine's nothing may be because of visaul studio
+os.system("ping -c 1 -w 2 8.8.8.8 > /dev/null 2>&1")
